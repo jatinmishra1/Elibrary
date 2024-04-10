d3b2bf8a1b0a3ff81334512a8b1e35564343040b
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-11 00:31:19 +0530

    setup expresss server

--- server.ts
+++ server.ts
@@ -2,3 +2,14 @@ console.log("book apis");
 function test() {
   console.log("h");
 }
+
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`server is running on port no ${port}`);
+  });
+};
+startServer();
