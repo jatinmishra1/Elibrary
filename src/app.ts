61f013c8982f97c6dcf165d08d33a361c81ae938
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-11 02:55:51 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
 const app = express();
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 //Routes
 
 app.get("/", (req, res, next) => {
+  //   throw new Error("something went wrong");
+  const error = createHttpError(400, "something went wrong");
+  throw error;
   res.json({ message: "this is your message" });
 });
+
+//global error handlers
+app.use(globalErrorHandler);
 
 export default app;
