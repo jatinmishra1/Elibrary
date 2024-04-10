4affdb46b5ed3e4348d08e419d7ee2aa941476ed
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-11 03:22:01 +0530

    add user Model

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //validation
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     const error = createHttpError(400, "all fiels are required");
+    return next(error);
+  }
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "already user exits");
     return next(error);
   }
 
