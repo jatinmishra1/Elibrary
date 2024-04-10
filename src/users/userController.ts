cd37ce60ba7671938eba195bf84097f450d4049e
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-11 03:26:14 +0530

    add password hash

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-
+import bcrypt from "bcrypt";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //validation
   const { name, email, password } = req.body;
@@ -14,6 +14,8 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "already user exits");
     return next(error);
   }
+  //password->hash
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   return res.json({ message: "from the cont" });
 };
