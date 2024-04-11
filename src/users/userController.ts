c60df4298b0ac850b3a64e5565b6c4a2e3e0cc5c
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-11 13:58:57 +0530

    add book model

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -42,10 +42,50 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       algorithm: "HS256",
     });
 
-    return res.json({ accessToken: token });
+    return res.status(201).json({ accessToken: token });
   } catch (err) {
     return next(createHttpError(500, "token generation failed"));
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  //validation
+  const { email, password } = req.body;
+  if (!email || !password) {
+    const error = createHttpError(400, "all fiels are required");
+    return next(error);
+  }
+  let user;
+  try {
+    user = await userModel.findOne({ email: email });
+    if (!user) {
+      const error = createHttpError(404, "user Not Find");
+      return next(error);
+    }
+  } catch (err) {
+    return next(createHttpError(500, "eroor while getting user"));
+  }
+  let isMatch;
+  try {
+    isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
+    if (!isMatch) {
+      return next(createHttpError(400, "username or password is incorrect"));
+    }
+  } catch (err) {
+    return next(createHttpError(400, "something went wrong"));
+  }
+
+  try {
+    const token = sign({ sub: user._id }, config.jwtSecretKey as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+    res.json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(404, "something went wrong"));
+  }
+  //create accesstoken
+};
+
+export { createUser, loginUser };
