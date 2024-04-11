7a6e9ef866cbbc13d97ce04ba283d61762b0210b
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-11 13:17:07 +0530

    add error handling

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
   port: process.env.PORT,
   dataBaseURL: process.env.MONGO_CONNECTION_URL,
   env: process.env.NODE_ENV,
+  jwtSecretKey: process.env.JWT_SECRET_KEY,
 };
 
 export const config = Object.freeze(_config);
--- src/users/userController.ts
+++ src/users/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "./userTypes";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //validation
   const { name, email, password } = req.body;
@@ -9,15 +12,40 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "all fiels are required");
     return next(error);
   }
-  const user = await userModel.findOne({ email: email });
-  if (user) {
-    const error = createHttpError(400, "already user exits");
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const error = createHttpError(400, "already user exits");
+      return next(error);
+    }
+  } catch (err) {
+    return next(createHttpError(500, "eroor while getting user"));
   }
+
   //password->hash
   const hashedPassword = await bcrypt.hash(password, 10);
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (err) {
+    return next(createHttpError(500, "error while creating user"));
+  }
 
-  return res.json({ message: "from the cont" });
+  try {
+    //token generation jwt
+    const token = sign({ sub: newUser._id }, config.jwtSecretKey as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    return res.json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(500, "token generation failed"));
+  }
 };
 
 export { createUser };
