fedf1c7279e2a7f918334660a030e05898684340
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-17 19:13:58 +0530

    setup next js project

--- src/users/userModel.ts
+++ src/users/userModel.ts
@@ -21,4 +21,9 @@ const userSchema = new mongoose.Schema<User>(
   { timestamps: true }
 );
 
+userSchema.pre("save", function () {
+  //this is technique in which basically we are trying to get the thing done
+  //when .save method will be called on userSchema  we want something to be done before that ,and then this will be used
+});
+
 export default mongoose.model<User>("User", userSchema);
