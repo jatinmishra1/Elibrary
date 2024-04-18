143c73f40c02638233d2c7e675a5badfce69c342
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-18 10:11:34 +0530

    Add Banner

--- frontend/elib-client/src/app/page.tsx
+++ frontend/elib-client/src/app/page.tsx
@@ -1,5 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>welcome</h1>;
+  return (
+    <>
+      <Banner />
+    </>
+  );
 }
