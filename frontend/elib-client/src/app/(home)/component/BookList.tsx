4578a7ac16274ccf8deafe9ebae48669efb855eb
jatin0103 <dbm.merndeveloper10@gmail.com>
2024-04-18 11:40:57 +0530

    add suspense

--- frontend/elib-client/src/app/(home)/component/BookList.tsx
+++ frontend/elib-client/src/app/(home)/component/BookList.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Book } from "@/types";
-import { BookCard } from "./BookCard";
 
-export function BookList({ books }: { books: Book[] }) {
+import { BookCard } from "./BookCard";
+import { Book } from "@/types";
+
+export async function BookList() {
+  const response = await fetch(`${process.env.BACKEND_URL}/books/`);
+  if (!response.ok) {
+    throw new Error("error while fecthing the books");
+  }
+  const books = await response.json();
+  console.log("books", books);
+
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-2 max-w-7xl mx-auto mb-10">
-      {books.map((book) => {
+      {books.map((book: Book) => {
         return (
           <>
             <BookCard key={book._id} book={book} />
--- frontend/elib-client/src/app/(home)/page.tsx
+++ frontend/elib-client/src/app/(home)/page.tsx
@@ -4,23 +4,20 @@ import Banner from "@/app/(home)/component/Banner";
 import Image from "next/image";
 import { BookList } from "./component/BookList";
 import { error } from "console";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 export default async function Home() {
   //data fetching
-
-  const response = await fetch(`${process.env.BACKEND_URL}/books/`);
-  if (!response.ok) {
-    throw new Error("error while fecthing the books");
-  }
-  const books = await response.json();
-  console.log("books", books);
 
   //server component,by default it is an server component
   //client component
   return (
     <>
       <Banner />
-      <BookList books={books} />
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
